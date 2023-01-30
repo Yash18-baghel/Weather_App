8e815c3af5273927aac06ea7c39266526b523b79
yash baghel <yashbaghel09680@gmail.com>
2023-01-30 12:01:07 +0530

    creating functionaltiy to get current data after a search

--- src/App.js
+++ src/App.js
@@ -8,38 +8,57 @@ function App() {
   const [weather, setWeather] = useState({})
   const [isLoading, setisLoading] = useState(true)
   const [isfound, setIsfound] = useState(true)
+  const [curCord, setCurCord] = useState({})
 
   function getFromSever(url) {
     setisLoading(true)
     console.log(url)
     try {
-      
+
       axios.get(url)
-      .then(res => {
-        console.log('res', res);
-        if (res.status === 404) {
-          throw res.status;
-        }
+        .then(res => {
+          console.log('res', res);
+          if (res.status === 404) {
+            throw res.status;
+          }
 
-        setWeather(res.data);
-        setisLoading(false)
-        setIsfound(true)
-      }).catch(err => {
-       setIsfound(false)
-       setisLoading(false)
+          setWeather(res.data);
+          setisLoading(false)
+          setIsfound(true)
+        }).catch(err => {
+          setIsfound(false)
+          setisLoading(false)
 
-      })
+        })
     }
     catch (err) {
       setIsfound(false)
       setisLoading(false)
     }
   }
+  const currenLocationData = () => {
+    if (curCord.latitude === undefined) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        getFromSever(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=240c128ac687fec539b91c01a0f58fda`)
+
+      })
+    }
+    else {
+      getFromSever(`https://api.openweathermap.org/data/2.5/weather?lat=${curCord.latitude}&lon=${curCord.longitude}&units=metric&appid=240c128ac687fec539b91c01a0f58fda`)
+    }
+  }
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude
       let lon = position.coords.longitude
-      getFromSever(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=240c128ac687fec539b91c01a0f58fda`)
+      setCurCord({
+        latitude: lat,
+        longitude: lon
+      })
+      currenLocationData();
+      // getFromSever(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=240c128ac687fec539b91c01a0f58fda`)
     });
   }, [])
   useEffect(() => {
@@ -50,11 +69,23 @@ function App() {
 
   return (
     <>
+      <div className="d-flex justify-content-d"
+        style={{
+          position:'absolute',
+          right:0,
+          top:0,
+          padding:"20px 25px"
+        }}
+      >
+        <button className='nearMe-btn ' onClick={currenLocationData}>
+          Near Me
+        </button>
+      </div>
       <div className="cards">
 
         <Search searchterm={(text) => setText(text)} />
-        {!isfound?(<div className="text-center mx-auto">No City Found</div>):''}
-        { (Object.entries(weather).length > 3 && isfound && !isLoading)  && (<Weather weather={weather} />) }
+        {!isfound ? (<div className="tex t-center mx-auto">No City Found</div>) : ''}
+        {(Object.entries(weather).length > 3 && isfound && !isLoading) && (<Weather weather={weather} />)}
         {isLoading && (
           <h1>Loading....</h1>
         )}
